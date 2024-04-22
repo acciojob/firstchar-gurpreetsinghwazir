794434cdc0efdaf8444b0b8500483d2bc02da8a2
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-04-22 19:16:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ function firstChar(text) {
   // your code here
 		let newtext = text.trim().split(" ");
     	
-    	if(newtext == ""){
+    	ifwords.length === 0 || words[0] === "")){
     	    	console.log(`""`);
     	}else{
     	    console.log(newtext[0][0]);
     	}
 }
+
 // Do not change the code below
 
 const text = prompt("Enter text:");
