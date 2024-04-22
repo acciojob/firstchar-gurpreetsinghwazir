2bcb0db3c0e7d59a2cd9e54e15bf04a5e5600a0b
Dragon_Fly <143097451+gurpreetsinghwazir@users.noreply.github.com>
2024-04-22 19:01:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
   // your code here
+		let newWrd = s.split(" ");
+	console.log(newWrd[0][0])
+
+
+
+
+	
 }
 
 // Do not change the code below
 
-const text = prompt("Enter text:");
-alert(firstChar(text));
+//const text = prompt("Enter text:");
+//alert(firstChar(text));
